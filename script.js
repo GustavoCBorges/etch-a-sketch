2df7e565a36345e65609411992cf8872ecef9f5e
Gustavo <gustavo.cordeiro.borges26@gmail.com>
2022-03-30 10:51:32 -0300

    Splitted the event listener out of the loop that creates the grid

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const container = document.querySelector('.container');
 for (i = 0; i < 256; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
-    div.addEventListener('mouseenter', () => {
-        div.classList.add('pixel');
-    })
-}
+    div.classList.add('gridElement');
+};
+
+const gridElement = document.querySelectorAll('.gridElement');
+gridElement.forEach((element) => {
+    element.addEventListener('mouseenter' , () => {
+        element.classList.add('pixel');
+        console.log(element.classList);
+    });
+});
