dadb2deb7347f09b86496a1c02cbb36f59a94051
Gustavo <gustavo.cordeiro.borges26@gmail.com>
2022-04-01 16:31:24 -0300

    Created rainbowMode function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const clear = document.querySelector('#clear');
 const buttons = document.querySelectorAll('.button');
 const color = document.querySelector('#color-selector');
+const rainbow = document.querySelector('#rainbow');
 
 createGrid(16);
 
@@ -13,7 +14,11 @@ function createGrid(gridSize) {
         div.classList.add('gridLine');
     };
     container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
-    makeGridDynamic();
+    if (rainbow.classList.contains('on') == true) {
+        rainbowMode();
+    } else {
+        makeGridDynamic();
+    }
 };
 
 function removeCurrentGrid() {
@@ -28,6 +33,13 @@ buttons.forEach((button) => {
             clearGrid();
         } else if (e.target.id == 'toggle-grid') {
             showGrid();
+        } else if (e.target.id == 'rainbow') {
+            rainbow.classList.toggle('on');
+            if (rainbow.classList.contains('on') == true) {
+                rainbowMode();
+            } else {
+                makeGridDynamic();
+            };
         } else {
             removeCurrentGrid();
             createGrid(e.target.id);
@@ -55,5 +67,22 @@ function clearGrid() {
     const gridElement = document.querySelectorAll('.gridElement');
     gridElement.forEach((element) => {
         element.style.backgroundColor = 'rgb(255,255,255)';
+    });
+};
+
+function selectRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let randomColor = `rgb(${r},${g},${b})`;
+    return randomColor
+}
+
+function rainbowMode() {
+    const gridElement = document.querySelectorAll('.gridElement');
+    gridElement.forEach((element) => {
+        element.addEventListener('mouseenter' , (e) => {
+            e.target.style.backgroundColor = selectRandomColor();
+        });
     });
 };
